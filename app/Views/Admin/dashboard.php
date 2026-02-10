16dcdfbcbafe98b63ecbc4dde1826bfedd923752
LAPTOP-52IU6IJS <martincliffordore16@gmail.com>
2026-02-11 02:13:39 +0800

    Merge branch 'main' of https://github.com/kulit129163/NextCafe-Coffee-Shop

--- app/Views/Admin/dashboard.php
+++ app/Views/Admin/dashboard.php
@@ -13,9 +13,6 @@
     <div class="sidebar">
         <div class="sidebar-brand">
             <img src="<?= base_url('images/logo.png') ?>" alt="NextCafe" class="brand-logo">
-            <div class="sidebar-toggle">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
-            </div>
         </div>
         
         <nav class="sidebar-nav">
